3fb43cf9ea2f5af36333debfccfb040f618b15ec
bosiarquitetura <bosiarquitetura@gmail.com>
2019-11-12 22:14:57 -0300

    Testing a pwa solution

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-(() => {
-
-    if ("serviceWorker" in navigator) {
-        if (navigator.serviceWorker.controller) {
-            console.log("[PWA Builder] active service worker found, no need to register");
-    } else {
-        // Register the service worker
-        navigator.serviceWorker
-            .register("pwabuilder-sw.js", {
-                scope: "./"
-            })
-            .then(function (reg) {
-                console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
-            });
-        }
-    } 
-})();
-
 let deferredPrompt = null;
 
 window.addEventListener('beforeinstallprompt', (e) => {
